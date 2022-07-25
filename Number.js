eb02df2f0426eb1f9c366996895aa23be410c2d5
Samulinkin <samulinkin@gmail.com>
2022-07-25 13:44:01 -0300

    Bolleano

--- Number.js
+++ Number.js
@@ -1,11 +1,18 @@
-const escola = 'Cod3er'
+const peso1 = 2.0
+const peso2 = Number('3.5')
 
-console.log(typeof escola)
+console.log(peso1,peso2)
+console.log(Number.isInteger(peso1))
+console.log(Number.isInteger(peso2))
+console.log(typeof peso2)
 
-console.log(escola.charAt(2))
+const avaliacao1 = 3.528
+const avaliacao2 = 2.354
+const total = peso1 * avaliacao1 + peso2 * avaliacao2
+const media = total / (peso1 + peso2)
 
-
-console.log(escola.substring(3))
-console.log(escola.substring(0,3))
-console.log(escola.indexOf('r'))
-console.log(typeof escola)
+console.log(media)
+console.log(media.toFixed(2))
+console.log(media.toString())
+console.log(Number.isInteger(media))
+console.log(typeof media)
